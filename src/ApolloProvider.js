df4768a7bbe5d295f9d10b5ddd9f92d8bc658236
Tantely Rambeloson <ftantely@gmail.com>
2020-09-09 15:53:57 -0600

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import {ApolloProvider} from '@apollo/react-hooks'
 import {setContext} from "apollo-link-context";
 
 const httpLink = createHttpLink({
-    uri : 'http://localhost:4444/'
+    uri : 'https://glacial-atoll-08363.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
